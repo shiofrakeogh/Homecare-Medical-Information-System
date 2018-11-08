a35ce0446ba59b993fb0b6de0051270297323c59
Shíofra Keogh <shiofrakeogh@yahoo.co.uk>
2018-11-08 17:47:19 +0000

    Add files via upload

---
+++ DSProject/src/dsproject/Register.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dsproject;
+import java.awt.*;
+import javax.swing.*;
+import tictactoegame.TTTWebService;
+import tictactoegame.TTTWebService_Service;
+
+/**
+ *
+ * @author Shíofra
+ */
+public class Register {
+    TTTWebService_Service link = new TTTWebService_Service();
+    TTTWebService proxy = link.getTTTWebServicePort();
+    
+    
+    
+}

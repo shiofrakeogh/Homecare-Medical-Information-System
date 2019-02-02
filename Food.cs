8d6752651ebb1c5305340bbc2a8ea5b2f049da4a
Shíofra Keogh <15145247@studentmail.ul.ie>
2019-02-02 20:50:20 +0000

    Add files via upload

---
+++ Food.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.WindowsAzure.MobileServices;
+using Newtonsoft.Json;
+
+namespace HMIS
+{
+    public class Food
+    {
+        string foodID;
+        string breakfast;
+        string lunch;
+        string dinner;
+        string snacks;
+
+        [JsonProperty(PropertyName = "foodID")]
+        public string foodid
+        {
+            get { return foodID; }
+            set { foodID = value; }
+        }
+
+        [JsonProperty(PropertyName = "breakfast")]
+        public string breakFast
+        {
+            get { return breakfast; }
+            set { breakfast = value; }
+        }
+
+        [JsonProperty(PropertyName = "lunch")]
+        public string lunch1
+        {
+            get { return lunch; }
+            set { lunch = value; }
+        }
+
+        [JsonProperty(PropertyName = "dinner")]
+        public string dinner1
+        {
+            get { return dinner; }
+            set { dinner = value; }
+        }
+
+        [JsonProperty(PropertyName = "snacks")]
+        public string snacks1
+        {
+            get { return snacks; }
+            set { snacks = value; }
+        }
+
+
+        [Version]
+        public string Version { get; set; }
+    }
+}
---
+++ FoodData.cs
@@ -0,0 +1,186 @@
+﻿/*
+ * To add Offline Sync Support:
+ *  1) Add the NuGet package Microsoft.Azure.Mobile.Client.SQLiteStore (and dependencies) to all client projects
+ *  2) Uncomment the #define OFFLINE_SYNC_ENABLED
+ *
+ * For more information, see: http://go.microsoft.com/fwlink/?LinkId=620342
+ */
+//#define OFFLINE_SYNC_ENABLED
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
+
+#if OFFLINE_SYNC_ENABLED
+using Microsoft.WindowsAzure.MobileServices.SQLiteStore;
+using Microsoft.WindowsAzure.MobileServices.Sync;
+#endif
+
+namespace HMIS
+{
+    public partial class FoodData
+    {
+        static FoodData defaultInstance = new FoodData();
+        MobileServiceClient client;
+
+#if OFFLINE_SYNC_ENABLED
+        IMobileServiceSyncTable<Food> foodTable;
+#else
+        IMobileServiceTable<Food> foodTable;
+#endif
+
+        const string offlineDbPath = @"localstore.db";
+
+        private FoodData()
+        {
+            this.client = new MobileServiceClient(Constants.ApplicationURL);
+
+#if OFFLINE_SYNC_ENABLED
+            var store = new MobileServiceSQLiteStore(offlineDbPath);
+            store.DefineTable<Food>();
+
+            //Initializes the SyncContext using the default IMobileServiceSyncHandler.
+            this.client.SyncContext.InitializeAsync(store);
+
+            this.foodTable = client.GetSyncTable<Food>();
+#else
+            this.foodTable = client.GetTable<Food>();
+#endif
+        }
+
+        public static FoodData DefaultManager
+        {
+            get
+            {
+                return defaultInstance;
+            }
+            private set
+            {
+                defaultInstance = value;
+            }
+        }
+
+        public MobileServiceClient CurrentClient
+        {
+            get { return client; }
+        }
+
+        public bool IsOfflineEnabled
+        {
+            get { return foodTable is Microsoft.WindowsAzure.MobileServices.Sync.IMobileServiceSyncTable<Food>; }
+        }
+
+        /*public async Task<ObservableCollection<Food>> GetFoodItemsAsync(bool syncItems = false)
+        {
+            try
+            {
+#if OFFLINE_SYNC_ENABLED
+                if (syncItems)
+                {
+                    await this.SyncAsync();
+                }
+#endif
+                IEnumerable<Food> items = await foodTable
+                    .Where(foodItem => !foodItem.Done)
+                    .ToEnumerableAsync();
+
+                return new ObservableCollection<Food>(items);
+            }
+            catch (MobileServiceInvalidOperationException msioe)
+            {
+                Debug.WriteLine("Invalid sync operation: {0}", new[] { msioe.Message });
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Sync error: {0}", new[] { e.Message });
+            }
+            return null;
+        }*/
+        public async Task DeleteTaskAsync(Food item)
+        {
+            try
+            {
+                await foodTable.DeleteAsync(item);
+            }
+            catch (MobileServiceInvalidOperationException msioe)
+            {
+                Debug.WriteLine(@"INVALID {0}", msioe.Message);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(@"ERROR {0}", e.Message);
+            }
+        }
+
+        public async Task SaveTaskAsync(Food item)
+        {
+            try
+            {
+                if (item.foodid == null)
+                {
+                    await foodTable.InsertAsync(item);
+                }
+                else
+                {
+                    await foodTable.UpdateAsync(item);
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Save error: {0}", new[] { e.Message });
+            }
+        }
+
+#if OFFLINE_SYNC_ENABLED
+        public async Task SyncAsync()
+        {
+            ReadOnlyCollection<MobileServiceTableOperationError> syncErrors = null;
+
+            try
+            {
+                await this.client.SyncContext.PushAsync();
+
+                await this.todoTable.PullAsync(
+                    //The first parameter is a query name that is used internally by the client SDK to implement incremental sync.
+                    //Use a different query name for each unique query in your program
+                    "allTodoItems",
+                    this.todoTable.CreateQuery());
+            }
+            catch (MobileServicePushFailedException exc)
+            {
+                if (exc.PushResult != null)
+                {
+                    syncErrors = exc.PushResult.Errors;
+                }
+            }
+
+            // Simple error/conflict handling. A real application would handle the various errors like network conditions,
+            // server conflicts and others via the IMobileServiceSyncHandler.
+            if (syncErrors != null)
+            {
+                foreach (var error in syncErrors)
+                {
+                    if (error.OperationKind == MobileServiceTableOperationKind.Update && error.Result != null)
+                    {
+                        //Update failed, reverting to server's copy.
+                        await error.CancelAndUpdateItemAsync(error.Result);
+                    }
+                    else
+                    {
+                        // Discard local change.
+                        await error.CancelAndDiscardItemAsync();
+                    }
+
+                    Debug.WriteLine(@"Error executing sync operation. Item: {0} ({1}). Operation discarded.", error.TableName, error.Item["id"]);
+                }
+            }
+        }
+#endif
+    }
+}
+
